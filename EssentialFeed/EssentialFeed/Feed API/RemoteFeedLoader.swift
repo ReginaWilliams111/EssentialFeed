337cfbd78d7f97851e6fb2934efc15ce1974e1d0
Regina <74955723+ReginaWilliams111@users.noreply.github.com>
2024-08-23 05:26:31 -0400

    Delivers connectivity error on client error

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,19 +8,27 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
+    
+    // since our type name is the same as swift, we can specify Swift.Error so it doesnt infer incorrectly.
+    public enum Error: Swift.Error {
+        case connectivity
+    }
         
     public init(url: URL, client: HTTPClient) {
         self.client = client
         self.url = url
     }
     
-    public func load() {
-        client.get(from: url)
+    // we are able to set a default closure here so we don't break other tests
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
